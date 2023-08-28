7da94fedbaacf43d954ecf7e5bf05ccd2b93401b
wanatthaphong.p <wanatthaphong.p>
2023-08-28 13:08:05 +0700

    28/08/2566

--- pages/cstomer/index.vue
+++ pages/cstomer/index.vue
@@ -1,6 +1,6 @@
 <template>
     <v-row>
-   Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ullam magni laborum enim esse maxime, quas in maiores minus doloribus illum rerum repellat recusandae officia, ipsam minima laudantium quidem. Vero.
+   
     </v-row>
   </template>
   
